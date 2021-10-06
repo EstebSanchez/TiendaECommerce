7b46dc0105a44e006a534f9953c1e36b3964de5b
Esteban Sanchez <estebansanchez2013@gmail.com>
2021-10-06 00:18:17 -0300

    Adding stock limit

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react'
 import './ItemCount.css'
-const ItemCount = (stock) => {
+const ItemCount = ({stock}) => {
 
-    console.log(stock)
     const [clicks, setClicks] = useState(1)
 
-    console.log(clicks)
-
-    const addClick = (stock) => {
-        if(clicks >= 0) setClicks(clicks+1)
+    const addClick = () => {
+        clicks >= 0 && clicks < stock && setClicks(clicks+1)
     }
 
-    const removeClick = (stock) => {
-        if (clicks > 0) setClicks(clicks-1)
+    const removeClick = () => {
+        clicks > 0 && clicks <= stock && setClicks(clicks-1)
     }
 
     return (
         <div className="counter">
-            <button onClick = {() => addClick()} className="btn">+</button>
-            <div className="count">{clicks}</div>
             <button onClick = {() => removeClick()} className="btn">-</button>
+            <div className="count">{clicks}</div>
+            <button onClick = {() => addClick()} className="btn">+</button>
         </div>
     )
 }
